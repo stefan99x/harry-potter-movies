bfd032eb2d7b870b4b086d46528e1c47a1f60f2d
Steve <g.stefan99x@gmail.com>
2024-07-13 11:35:00 +0300

    Added pipes and icon for going back

--- src/app/components/movie-details-table/movie-details-table.component.ts
+++ src/app/components/movie-details-table/movie-details-table.component.ts
@@ -1,12 +1,15 @@
 import {Component, input, InputSignal} from '@angular/core';
 import {MovieModel} from "../../models/movie.model";
-import {NgForOf} from "@angular/common";
+import {CurrencyPipe, NgForOf} from "@angular/common";
+import {MinutesToHoursPipe} from "../../Pipes/minutes-to-hours.pipe";
 
 @Component({
   selector: 'app-movie-details-table',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    CurrencyPipe,
+    MinutesToHoursPipe
   ],
   templateUrl: './movie-details-table.component.html',
   styleUrl: './movie-details-table.component.css'
